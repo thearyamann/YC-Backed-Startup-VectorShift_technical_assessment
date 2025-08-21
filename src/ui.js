abb8e738740014c33e8429d4068c5d688cce8620
Aryamann Chaudhary <thearyamann15151@gmail.com>
2025-08-21 19:13:24 +0530

    ui.js update

--- src/ui.js
+++ src/ui.js
@@ -6,7 +6,7 @@ import { useState, useRef, useCallback } from "react";
 import ReactFlow, { Controls, Background, MiniMap } from "reactflow";
 import { useStore } from "./store";
 import { shallow } from "zustand/shallow";
-import { InputNode } from "./nodes/inputNode";
+import { InputNode } from "./nodes/InputNode";
 import { LLMNode } from "./nodes/llmNode";
 import { OutputNode } from "./nodes/outputNode";
 import "reactflow/dist/style.css";
